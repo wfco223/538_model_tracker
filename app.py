99d2772d49360e35bda0640f1df78b6d88ebe66d
wfco223 <38415099+wfco223@users.noreply.github.com>
2020-08-24 14:40:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,65 +3,66 @@ app = Flask(__name__)
 
 @app.route('/')
 
-import pandas as pd
-import matplotlib.pyplot as plt
-import datetime
-import numpy as np
-import os
-
-files = sorted(os.listdir('/ev_probs'))
-
-if pd.read_csv('/ev_probs/' + files[-1])['timestamp'][0] != pd.read_csv("https://projects.fivethirtyeight.com/2020-general-data/presidential_ev_probabilities_2020.csv")['timestamp'][0]:
-  df = pd.read_csv("https://projects.fivethirtyeight.com/2020-general-data/presidential_ev_probabilities_2020.csv")
-  df = df.sort_values(by = 'total_ev')
-
-  combined = pd.DataFrame({'biden_probs': df['evprob_chal'], 'trump_probs': df['evprob_inc']})
-  combined.head()
-
-  dt = datetime.datetime.today()
-  df.to_csv('/ev_probs/' + str(dt) + '.csv')
+def app():
+  import pandas as pd
+  import matplotlib.pyplot as plt
+  import datetime
+  import numpy as np
+  import os
 
   files = sorted(os.listdir('/ev_probs'))
-  
-  print('new', files[-1][:16])
-else:
-  latest_dt = datetime.datetime.strptime(files[-1][:-4], '%Y-%m-%d %H:%M:%S.%f')
-  delta = datetime.datetime.now() - latest_dt
-  if delta.total_seconds() // 3600 < 1:
-    print('Done. Most recent update', round(delta.total_seconds()/60), 'minutes ago')
+
+  if pd.read_csv('/ev_probs/' + files[-1])['timestamp'][0] != pd.read_csv("https://projects.fivethirtyeight.com/2020-general-data/presidential_ev_probabilities_2020.csv")['timestamp'][0]:
+    df = pd.read_csv("https://projects.fivethirtyeight.com/2020-general-data/presidential_ev_probabilities_2020.csv")
+    df = df.sort_values(by = 'total_ev')
+
+    combined = pd.DataFrame({'biden_probs': df['evprob_chal'], 'trump_probs': df['evprob_inc']})
+    combined.head()
+
+    dt = datetime.datetime.today()
+    df.to_csv('/ev_probs/' + str(dt) + '.csv')
+
+    files = sorted(os.listdir('/ev_probs'))
+
+    print('new', files[-1][:16])
   else:
-    print('Done. Most recent update', round(delta.total_seconds() // 3600), 'hours and', round((delta.total_seconds() % 3600)/60), 'minutes ago')
-    
-df1 = pd.read_csv('/ev_probs/' + files[1])
-df2 = pd.read_csv('/ev_probs/' + files[-1])
-differences = pd.DataFrame({'total_ev': df2['total_ev'], 'differences': df2['evprob_inc'] - df2['evprob_chal']})
-differences = differences.tail(269)
+    latest_dt = datetime.datetime.strptime(files[-1][:-4], '%Y-%m-%d %H:%M:%S.%f')
+    delta = datetime.datetime.now() - latest_dt
+    if delta.total_seconds() // 3600 < 1:
+      print('Done. Most recent update', round(delta.total_seconds()/60), 'minutes ago')
+    else:
+      print('Done. Most recent update', round(delta.total_seconds() // 3600), 'hours and', round((delta.total_seconds() % 3600)/60), 'minutes ago')
 
-differences_old = pd.DataFrame({'total_ev': df1['total_ev'], 'differences': df1['evprob_inc'] - df1['evprob_chal'], 'alpha': df1['evprob_inc'] + df1['evprob_chal']}).tail(269)
-differences_new = pd.DataFrame({'total_ev': df2['total_ev'], 'differences': df2['evprob_inc'] - df2['evprob_chal'], 'alpha': df2['evprob_inc'] + df2['evprob_chal']}).tail(269)
+  df1 = pd.read_csv('/ev_probs/' + files[1])
+  df2 = pd.read_csv('/ev_probs/' + files[-1])
+  differences = pd.DataFrame({'total_ev': df2['total_ev'], 'differences': df2['evprob_inc'] - df2['evprob_chal']})
+  differences = differences.tail(269)
 
-x_arr_old = differences_old['differences']
-x_arr_new = differences_new['differences'] 
-y_arr = differences_new['total_ev']
+  differences_old = pd.DataFrame({'total_ev': df1['total_ev'], 'differences': df1['evprob_inc'] - df1['evprob_chal'], 'alpha': df1['evprob_inc'] + df1['evprob_chal']}).tail(269)
+  differences_new = pd.DataFrame({'total_ev': df2['total_ev'], 'differences': df2['evprob_inc'] - df2['evprob_chal'], 'alpha': df2['evprob_inc'] + df2['evprob_chal']}).tail(269)
 
-plt.figure(figsize=(10, 20))
+  x_arr_old = differences_old['differences']
+  x_arr_new = differences_new['differences'] 
+  y_arr = differences_new['total_ev']
 
-for (x1, x2, y, alpha) in zip(x_arr_old, x_arr_new, y_arr, differences_new['alpha']):
-    if x2 < x1:
-      plt.plot([x1, x2], [y, y], '-', color='blue', alpha = alpha/differences_new['alpha'].max())
-    else: 
-      plt.plot([x1, x2], [y, y], '-', color='red', alpha = alpha/differences_new['alpha'].max())
+  plt.figure(figsize=(10, 20))
 
-    if x2 < 0:
-      plt.plot(x2, y, 'o', color = 'blue', alpha = alpha/differences_new['alpha'].max())
-    else: 
-      plt.plot(x2, y, 'o', color = 'red', alpha = alpha/differences_new['alpha'].max())
+  for (x1, x2, y, alpha) in zip(x_arr_old, x_arr_new, y_arr, differences_new['alpha']):
+      if x2 < x1:
+        plt.plot([x1, x2], [y, y], '-', color='blue', alpha = alpha/differences_new['alpha'].max())
+      else: 
+        plt.plot([x1, x2], [y, y], '-', color='red', alpha = alpha/differences_new['alpha'].max())
 
-plt.plot([0, 0], [269, 538], '--')
-plt.plot([x_arr_new.min(), x_arr_old.max()], [269, 269], '-', color = 'black')
+      if x2 < 0:
+        plt.plot(x2, y, 'o', color = 'blue', alpha = alpha/differences_new['alpha'].max())
+      else: 
+        plt.plot(x2, y, 'o', color = 'red', alpha = alpha/differences_new['alpha'].max())
 
-plt.show()
+  plt.plot([0, 0], [269, 538], '--')
+  plt.plot([x_arr_new.min(), x_arr_old.max()], [269, 269], '-', color = 'black')
 
-pre = datetime.datetime.strptime(files[1][:-4], '%Y-%m-%d %H:%M:%S.%f')
-post = datetime.datetime.strptime(files[-1][:-4], '%Y-%m-%d %H:%M:%S.%f')
-print('pre:  ', pre, '\n', 'post: ', post, '\n', post - pre, sep = '')
+  plt.show()
+
+  pre = datetime.datetime.strptime(files[1][:-4], '%Y-%m-%d %H:%M:%S.%f')
+  post = datetime.datetime.strptime(files[-1][:-4], '%Y-%m-%d %H:%M:%S.%f')
+  print('pre:  ', pre, '\n', 'post: ', post, '\n', post - pre, sep = '')
