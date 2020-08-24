e42659c022f603cbcc91b7b8df8d9a992d081384
wfco223 <38415099+wfco223@users.noreply.github.com>
2020-08-24 17:44:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
-from flask import Flask
+from flask import Flask, Response, render_template
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+import datetime
+import numpy as np
+import os
+import io
+
 app = Flask(__name__)
 
-@app.route('/')
+files = sorted(os.listdir('./total_ev'))
 
-def app(arg_1, arg_2):
-
-  import pandas as pd
-  import matplotlib.pyplot as plt
-  import datetime
-  import numpy as np
-  import os
-
-  files = sorted(os.listdir('./total_ev'))
-
-  if pd.read_csv('./total_ev/' + files[-1])['timestamp'][0] != pd.read_csv("https://projects.fivethirtyeight.com/2020-general-data/presidential_ev_probabilities_2020.csv")['timestamp'][0]:
+if pd.read_csv('./total_ev/' + files[-1])['timestamp'][0] != pd.read_csv("https://projects.fivethirtyeight.com/2020-general-data/presidential_ev_probabilities_2020.csv")['timestamp'][0]:
     df = pd.read_csv("https://projects.fivethirtyeight.com/2020-general-data/presidential_ev_probabilities_2020.csv")
     df = df.sort_values(by = 'total_ev')
 
@@ -34,36 +33,40 @@ def app(arg_1, arg_2):
     else:
       print('Done. Most recent update', round(delta.total_seconds() // 3600), 'hours and', round((delta.total_seconds() % 3600)/60), 'minutes ago')
 
-  df1 = pd.read_csv('./total_ev/' + files[1])
-  df2 = pd.read_csv('./total_ev/' + files[-1])
-  differences = pd.DataFrame({'total_ev': df2['total_ev'], 'differences': df2['evprob_inc'] - df2['evprob_chal']})
-  differences = differences.tail(269)
+df1 = pd.read_csv('./total_ev/' + files[1])
+df2 = pd.read_csv('./total_ev/' + files[-1])
 
-  differences_old = pd.DataFrame({'total_ev': df1['total_ev'], 'differences': df1['evprob_inc'] - df1['evprob_chal'], 'alpha': df1['evprob_inc'] + df1['evprob_chal']}).tail(269)
-  differences_new = pd.DataFrame({'total_ev': df2['total_ev'], 'differences': df2['evprob_inc'] - df2['evprob_chal'], 'alpha': df2['evprob_inc'] + df2['evprob_chal']}).tail(269)
+differences_old = pd.DataFrame({'total_ev': df1['total_ev'], 'differences': df1['evprob_inc'] - df1['evprob_chal'], 'alpha': df1['evprob_inc'] + df1['evprob_chal']}).tail(269)
+differences_new = pd.DataFrame({'total_ev': df2['total_ev'], 'differences': df2['evprob_inc'] - df2['evprob_chal'], 'alpha': df2['evprob_inc'] + df2['evprob_chal']}).tail(269)
 
-  x_arr_old = differences_old['differences']
-  x_arr_new = differences_new['differences'] 
-  y_arr = differences_new['total_ev']
+x_arr_old = differences_old['differences']
+x_arr_new = differences_new['differences'] 
+y_arr = differences_new['total_ev']
 
-  fig = plt.figure(figsize=(10, 20))
 
-  for (x1, x2, y, alpha) in zip(x_arr_old, x_arr_new, y_arr, differences_new['alpha']):
+
+plt.plot([0, 0], [269, 538], '--')
+plt.plot([x_arr_new.min(), x_arr_old.max()], [269, 269], '-', color = 'black')
+
+@app.route('/')
+
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
+
+def create_figure():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    for (x1, x2, y, alpha) in zip(x_arr_old, x_arr_new, y_arr, differences_new['alpha']):
       if x2 < x1:
-        plt.plot([x1, x2], [y, y], '-', color='blue', alpha = alpha/differences_new['alpha'].max())
+        axis.plot([x1, x2], [y, y], '-', color='blue', alpha = alpha/differences_new['alpha'].max())
       else: 
-        plt.plot([x1, x2], [y, y], '-', color='red', alpha = alpha/differences_new['alpha'].max())
+        axis.plot([x1, x2], [y, y], '-', color='red', alpha = alpha/differences_new['alpha'].max())
 
       if x2 < 0:
-        plt.plot(x2, y, 'o', color = 'blue', alpha = alpha/differences_new['alpha'].max())
+        axis.plot(x2, y, 'o', color = 'blue', alpha = alpha/differences_new['alpha'].max())
       else: 
-        plt.plot(x2, y, 'o', color = 'red', alpha = alpha/differences_new['alpha'].max())
-
-  plt.plot([0, 0], [269, 538], '--')
-  plt.plot([x_arr_new.min(), x_arr_old.max()], [269, 269], '-', color = 'black')
-
-  pre = datetime.datetime.strptime(files[1][:-4], '%Y-%m-%d %H_%M_%S.%f')
-  post = datetime.datetime.strptime(files[-1][:-4], '%Y-%m-%d %H_%M_%S.%f')
-  
-  message = "pre:  " + str(pre) + '\n' + 'post: ' + str(post) + "\n" + str(post - pre)
-  return [bytes(message, 'utf-8')]
+        axis.plot(x2, y, 'o', color = 'red', alpha = alpha/differences_new['alpha'].max())
+    return fig
