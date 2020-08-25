b6c120a1931607abadfc5297bb756b3f203c41d3
wfco223 <38415099+wfco223@users.noreply.github.com>
2020-08-25 06:57:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def update_probs():
         latest_dt = datetime.datetime.strptime(files[-1][:-4], '%b %d %Y %H:%M:%S')
         delta = datetime.datetime.now() - latest_dt
         if delta.total_seconds() // 3600 < 1:
-            message = ('Done. Most recent update ' + str(round(delta.total_seconds()/60)) + ' minutes ago.', '/var/data/probs/' + files[-1] + '.csv')
+            message = ('Done. Most recent update ' + str(round(delta.total_seconds()/60)) + ' minutes ago.', '/var/data/probs/' + files[-1])
         else:
             message = ('Done. Most recent update ' + str(round(delta.total_seconds() // 3600)) + ' hours and ' + str(round((delta.total_seconds() % 3600)/60)) + ' minutes ago.', '/var/data/probs/' + files[-1] + '.csv')
     
